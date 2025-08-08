dea436658478541f2c64db8ca4edcf3682e1e5b2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-08-08 11:17:18 +0200

    Fixed food book not counting as book

--- src/main/java/team/creative/solonion/common/item/FoodBookItem.java
+++ src/main/java/team/creative/solonion/common/item/FoodBookItem.java
@@ -4,14 +4,14 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import team.creative.solonion.client.gui.screen.FoodBookScreen;
 
-public final class FoodBookItem extends Item {
+public final class FoodBookItem extends BookItem {
     public FoodBookItem() {
         super(new Properties());
     }
